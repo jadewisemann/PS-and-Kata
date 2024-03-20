dceaae9f13f2226a2af4837fada91300ab3659b0
jadewisemann <jadewisemann@gmail.com>
2024-03-20 23:09:53 +0900

    new solution: use switch pattern

--- codewars/Who_likes_it.js
+++ codewars/Who_likes_it.js
@@ -15,3 +15,18 @@ It must return the display text as shown in the examples:
 
 Note: For 4 or more names, the number in "and 2 others" simply increases.
 */ 
+
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
