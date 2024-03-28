9ad1084b5f7aa14a499cc121dbe4663a6f993a22
jadewisemann <jadewisemann@gmail.com>
2024-03-28 17:54:28 +0900

    mode solution: style to arrow function

--- codewars/your_order_please.js
+++ codewars/your_order_please.js
@@ -1,8 +1,6 @@
-function order(words){
-  if (!words) ""
-  return words
-    .split(' ')
-    .sort((a, b) => a.match(/\d/) - b.match(/\d/))
-    .join(' ');
-}
+order = words => !words ? words : words
+  .split(' ')
+  .sort((a, b) => a.match(/\d/) - b.match(/\d/))
+  .join(' ');
 
+console.log(newOrder("is2 Thi1s T4est 3a"))
