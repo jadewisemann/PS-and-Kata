0aa855e5a6a06b76bf3337ef11abd6ed90878787
jadewisemann <jadewisemann@gmail.com>
2024-03-30 23:15:05 +0900

    solution: do step by step method chaining

--- codewars/convert-string-to-camel-case.js
+++ codewars/convert-string-to-camel-case.js
@@ -1,3 +1,4 @@
-function toCamelCase(string) {
-  // your code
-}
+const toCamelCase = str => str
+  .split(/_|-/)
+  .map((word, idx) => idx === 0 ? word : word[0].toUpperCase() + word.slice(1))
+  .join('')
