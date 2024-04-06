f1180800fa2baeed45d34fe9570782a4b746c457
jadewisemann <jadewisemann@gmail.com>
2024-04-06 23:18:01 +0900

    new solution: try 2, answer 1.

--- programmers/문자열-나누기.py
+++ programmers/문자열-나누기.py
@@ -1,36 +1,26 @@
 def solution(s):
     answer = 0
-    criteria = ""
-    criteria_counter = 0
-    dict_counter = {}
     
-    for  idx, letter in enumerate(s):
-        # 1> 기준 없음 => letter 가 기준
-        if not criteria:  
-            criteria = letter
-            criteria_counter += 1
-        # 2> 기준 있음
-
-        # 2> a> 기준 개수 측정
-        elif letter == criteria:
-            criteria_counter += 1
+    while s:
+        X = s[0]
+        is_X = 0
+        not_X = 0
         
-        # 2> b> 기준 왜 문자의 개수 측정 
-        else:
-            # 해당 문자가 있는지 확인
-            # 없다면, 카운터를 1로 선언
-            if letter in dict_counter.keys():
-                dict_counter[letter] += 1
-            # 있다면, 1추가
-            else:    
-                dict_counter[letter] = 1
-            # 기준이랑 개수 비교
-            if dict_counter[letter] == criteria_counter:
-                # 정답 추가
-                answer += 1
-                # 초기화
-                criteria = ""
-                criteria_counter = 0
-                dict_counter = {}
+        for idx,  letter in enumerate(s):
 
+            if letter == X:
+                is_X += 1
+            else:
+                not_X += 1
+            
+            if is_X == not_X:
+                answer += 1
+                s = s[idx+1:]
+                break
+        
+        else:
+            if is_X != not_X:
+                answer += 1
+            break
+        
     return answer
