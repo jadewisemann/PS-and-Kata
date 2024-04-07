815b6eb211e25db0197f7a5b3c865458e5b0618f
jadewisemann <jadewisemann@gmail.com>
2024-04-07 23:13:27 +0900

    solution #1: edit morder and more shorten code

--- codewars/gap-in-primes.js
+++ codewars/gap-in-primes.js
@@ -1,23 +1,18 @@
-function isPrime(num) {
-    for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
-        if (num % i === 0) return false;
-    }
-    return num > 1;
-}
-
 function gap(g, m, n) {
-    let lastPrime = 0;
-    for (let i = m; i <= n; i++) {
-        if (isPrime(i)) {
-            if (i - lastPrime === g) return [lastPrime, i];
-            lastPrime = i;
-        }
-    }
-    return null; // 조건에 맞는 소수 쌍이 없을 경우
-}
 
-// 예시 사용법
-console.log(gap(2, 5, 7)); // [5, 7]
-console.log(gap(2, 5, 5)); // null
-console.log(gap(4, 130, 200)); // [163, 167]
-console.log(gap(6, 100, 110)); // null
+  var lastPrime = 0;
+  
+  const isPrime = num => { 
+    for (var i = 2; ( i ** 2 ) <= num; i++) {
+      if (num % i == 0) return false;
+    } return true;
+  }
+  
+  for (var number = m; number <= n; number++)
+    if (isPrime(number)) { 
+      if (number - lastPrime == g) return [lastPrime, number];
+      else lastPrime = number;
+    }
+  
+  return null;
+}
