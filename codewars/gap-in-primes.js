de0b85baf419bb2a416240344d339c71e6a9b5fb
jadewisemann <jadewisemann@gmail.com>
2024-04-07 21:58:24 +0900

    new solution #1

--- codewars/gap-in-primes.js
+++ codewars/gap-in-primes.js
@@ -1,34 +1,23 @@
+function isPrime(num) {
+    for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+        if (num % i === 0) return false;
+    }
+    return num > 1;
+}
+
 function gap(g, m, n) {
-  const isPrime = findPrimes(n)
-  primesInRange = []
-  
-  for (let index = m; m <= index <= n; index++) {
-    if (isPrime[index]) {
-      primesInRange.push(index);
+    let lastPrime = 0;
+    for (let i = m; i <= n; i++) {
+        if (isPrime(i)) {
+            if (i - lastPrime === g) return [lastPrime, i];
+            lastPrime = i;
+        }
     }
-  }
-  primesInRange = Set(primesInRange)
-  for (let prime of primesInRange) {
-    if (primesInRange.has(prime + g)) {
-      return [prime, prime + g]
-    } 
-  }
+    return null; // 조건에 맞는 소수 쌍이 없을 경우
 }
 
-function findPrimes(targetNumber) {
-  // 소수인지 확인하는 배열, 인덱스는 숫자고, 0, 1은 아님
-  const isPrimes =  [ false, false, ...Array(targetNumber - 1).fill(true)];
-  const primes = [];
-  const squareRoot = Math.sqrt(targetNumber) 
-
-  // 2에서 타겟 숫자의 제곱근까지의 모든 숫자를 전부 순회
-  for (let number = 2; number <= squareRoot; number++) {
-    // 해당 숫자가 소수면, 그 숫자의 배수들을 모두 소수가 아니라고 표시
-    if (isPrimes[number]) {
-      for (let multiple = number * number; multiple <= targetNumber; multiple += number) {
-        isPrimes[multiple] = false;
-      }
-    }
-  }
-  return isPrimes;
-}
+// 예시 사용법
+console.log(gap(2, 5, 7)); // [5, 7]
+console.log(gap(2, 5, 5)); // null
+console.log(gap(4, 130, 200)); // [163, 167]
+console.log(gap(6, 100, 110)); // null
