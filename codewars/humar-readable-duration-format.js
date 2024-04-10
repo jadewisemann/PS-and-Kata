606cb1967d380b967463bf5739bb98e3b80bcfbb
jadewisemann <jadewisemann@gmail.com>
2024-04-10 22:47:40 +0900

    solution: add [#2] use method chainring and reduce to avoid hard coding

--- codewars/humar-readable-duration-format.js
+++ codewars/humar-readable-duration-format.js
@@ -53,19 +53,35 @@ Formally, the duration specified by of a component must not be greater than any 
 
 //
 
+const TIME_UNITS = {
+  second: 1,
+  minute: 60,
+  hour: 60,
+  day: 24,
+  year: 365
+}
+
 const formatDuration = seconds => {
   const modular = (num, divisor) => [Math.floor(num / divisor), num % divisor];
-
-  const [tmp_minute, second] = modular(seconds, 60);
-  const [tmp_hour, minute] = modular(tmp_minute, 60);
-  const [tmp_day, hour] = modular(tmp_hour, 24);
-  const [year, day] = modular(tmp_day, 365);
-
-  const units = [[year, "year"], [day, "day"], [hour, "hour"], [minute, "minute"], [second, "second"]];
-
-  const result = units
-    .filter(([value, _]) => value > 0)
-    .map(([value, label]) => `${value} ${label}${value > 1 ? 's' : ''}`);
   
-  return result.length > 1 ?`${result.slice(0, -1).join(", ")} and ${result[result.length - 1]}`: result[0] || "now";
-};
+  const result = Object.entries(TIME_UNITS)
+    .reduce(([arr, acc], [key, val]) => {
+      acc *= val;
+      arr.push([key, acc]);
+      return [arr, acc]
+    }, [[], 1])[0]
+    .reverse()
+    .reduce(([arr, acc], [key, val]) => {
+      if (seconds >= val) {
+        acc = Math.floor(seconds / val);
+        arr.push([key, acc])
+        seconds %= val
+      } return [arr, acc]
+    }, [[], 1])[0]
+    .filter(([_, val]) => val > 0)
+    .map(([key, val]) => `${val} ${key}${val > 1 ? 's' : ''}`);
+  
+  return result.length > 1
+    ? `${result.slice(0, -1).join(", ")} and ${result[result.length - 1]}`
+    : result[0] || "now";
+};  
