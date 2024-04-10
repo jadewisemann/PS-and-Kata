6687124283d7ce958d72bc4b57b809f5b85653dd
jadewisemann <jadewisemann@gmail.com>
2024-04-10 19:23:13 +0900

    solution: [#1] use hard coding on time units

--- codewars/humar-readable-duration-format.js
+++ codewars/humar-readable-duration-format.js
@@ -53,6 +53,19 @@ Formally, the duration specified by of a component must not be greater than any 
 
 //
 
-function formatDuration (seconds) {
-  // Complete this function
-}
+const formatDuration = seconds => {
+  const modular = (num, divisor) => [Math.floor(num / divisor), num % divisor];
+
+  const [tmp_minute, second] = modular(seconds, 60);
+  const [tmp_hour, minute] = modular(tmp_minute, 60);
+  const [tmp_day, hour] = modular(tmp_hour, 24);
+  const [year, day] = modular(tmp_day, 365);
+
+  const units = [[year, "year"], [day, "day"], [hour, "hour"], [minute, "minute"], [second, "second"]];
+
+  const result = units
+    .filter(([value, _]) => value > 0)
+    .map(([value, label]) => `${value} ${label}${value > 1 ? 's' : ''}`);
+  
+  return result.length > 1 ?`${result.slice(0, -1).join(", ")} and ${result[result.length - 1]}`: result[0] || "now";
+};
