aa60e078ddd8e51dcc95f54c3c792cd85c162a62
jadewisemann <jadewisemann@gmail.com>
2024-04-12 22:41:42 +0900

    quesiton: add new

---
+++ codewars/sum-strings-as-number.js
@@ -0,0 +1,26 @@
+/*
+
+```yaml
+problem: "Sudoku Solver"
+tags: string, big-integers, algorithms
+difficulty: 4-kyu
+source: codewars
+link: https://www.codewars.com/kata/5324945e2ece5e1f32000370
+```
+
+Given the string representations of two integers,
+return the string representation of the sum of those integers.
+
+For example:
+
+```javascript
+sumStrings('1','2') // => '3'
+```
+
+A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+
+*/
+
+function sumStrings(a,b) { 
+  
+}
