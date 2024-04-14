298b033006e2ea2906e876ad5e85d719f5393799
jadewisemann <jadewisemann@gmail.com>
2024-04-14 21:36:27 +0900

    style: change to arrow functuon and more minial;

--- codewars/the-millionth-fibonacci-kata.js
+++ codewars/the-millionth-fibonacci-kata.js
@@ -59,7 +59,38 @@ Use this to reason what value `fib` has to have for negative values.
 
 */
 
+const fib = n => {
 
-function fib(n) {
-  return 0n;
+  const matrixMultiply = (a, b) => {
+    return [
+      [(a[0][0] * b[0][0] + a[0][1] * b[1][0]), (a[0][0] * b[0][1] + a[0][1] * b[1][1])],
+      [(a[1][0] * b[0][0] + a[1][1] * b[1][0]), (a[1][0] * b[0][1] + a[1][1] * b[1][1])]
+    ];
+  }
+
+  const matrixPower = (matrix, n) => 
+    n === 1 ? matrix :
+    n % 2 === 0 ? (() => {
+      const halfPower = matrixPower(matrix, n / 2)
+      return matrixMultiply(halfPower, halfPower)
+    }) () : matrixMultiply(matrix, matrixPower(matrix, n - 1))
+
+  if (n === 0) return BigInt(0);
+
+  let isNegative = false;
+  if (n < 0) {
+    isNegative = true;
+    n = -n
+  }
+
+  const resultMatrix = matrixPower([
+    [BigInt(1), BigInt(1)],
+    [BigInt(1), BigInt(0)]
+  ], n);
+
+  let result = resultMatrix[1][0];
+
+  if (isNegative && n % 2 === 0) result = -result;
+
+  return result;
 }
