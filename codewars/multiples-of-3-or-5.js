fd90bd1db872c35e1b483e8e14442a7ebb4ac780
jadewisemann <jadewisemann@gmail.com>
2024-04-15 19:32:21 +0900

    solution: add [#2] use method chaining

--- codewars/multiples-of-3-or-5.js
+++ codewars/multiples-of-3-or-5.js
@@ -18,12 +18,24 @@ Additionally, if the number is negative, return 0.
 **Note:** If the number is a multiple of **both** 3 and 5, only count it *once*.
 */
 
-const solution = number => {
-  let sum = 0;
-  for (let i = 1; i < number; i++) {
-    if ((i % 3 === 0) || (i % 5 === 0)) {
-      sum += i;
-    }
-  }
-  return sum
-}
+// const solution = number => {
+//   let sum = 0;
+//   for (let i = 1; i < number; i++) {
+//     if ((i % 3 === 0) || (i % 5 === 0)) {
+//       sum += i;
+//     }
+//   }
+//   return sum
+// }
+
+
+
+const solution = number => (number < 1 ) ? -1 : 
+  new Array(number - 1)
+    .fill(0)
+    .map((_, i) => i + 1)
+    .reduce((acc, curr) => {
+      if ((curr % 3 === 0 ) || (curr % 5 === 0)) {
+        acc += curr
+      } return acc
+    }, 0)
