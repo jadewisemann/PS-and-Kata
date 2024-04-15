0d9e57025aeb61d4905b11a5706533365fcdf9d3
jadewisemann <jadewisemann@gmail.com>
2024-04-15 19:19:26 +0900

    solution: add [#1] use for

--- codewars/multiples-of-3-or-5.js
+++ codewars/multiples-of-3-or-5.js
@@ -16,4 +16,14 @@ Finish the solution so that it returns the sum of all the multiples of 3 or 5 **
 Additionally, if the number is negative, return 0.
 
 **Note:** If the number is a multiple of **both** 3 and 5, only count it *once*.
-*/ 
+*/
+
+const solution = number => {
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if ((i % 3 === 0) || (i % 5 === 0)) {
+      sum += i;
+    }
+  }
+  return sum
+}
