9acee4cdf252e133abc71a6785d76ae95c996d05
jadewisemann <jadewisemann@gmail.com>
2024-04-16 15:13:30 +0900

    add new question
    ```yaml
    problem: "Sum of Digits / Digital Root"
    tags: MATHEMATICS, ALGORITHMS
    difficulty: 6-kyu
    source: codewars
    link: https://www.codewars.com/kata/541c8630095125aba6000c00
    ```

---
+++ codewars/8-kyu/sum-of-digits--digital-root.js
@@ -0,0 +1,12 @@
+/*
+```yaml
+problem: "Sum of Digits / Digital Root"
+tags: MATHEMATICS, ALGORITHMS
+difficulty: 6-kyu
+source: codewars
+link: https://www.codewars.com/kata/541c8630095125aba6000c00
+```
+*/
+function digitalRoot(n) {
+  // ...
+}
