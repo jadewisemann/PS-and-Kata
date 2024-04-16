cf8f7a14c6fbf950f9d54e2fa30b83ea4a322859
jadewisemann <jadewisemann@gmail.com>
2024-04-16 15:37:03 +0900

    solution: add[#2] use mathmatics

--- codewars/8-kyu/sum-of-digits--digital-root.js
+++ codewars/8-kyu/sum-of-digits--digital-root.js
@@ -7,11 +7,4 @@ source: codewars
 link: https://www.codewars.com/kata/541c8630095125aba6000c00
 ```
 */
-const digitalRoot = n => {
-  let result = 0
-  while (n > 0) {
-    result += n % 10
-    n = Math.floor(n/10)
-  }
-  return result >= 10 ? digitalRoot(result) : result 
-}
+const digitalRoot = n => (n - 1) % 9 + 1;
