c18597f55377e6919806f9e607656de429489048
jadewisemann <jadewisemann@gmail.com>
2024-04-17 11:19:35 +0900

    solution: add new
    ```yaml
    problem: "숫자 타자 대회"
    tags:
    difficulty: LV3
    source: programmers
    link: https://school.programmers.co.kr/learn/courses/30/lessons/136797
    ```

---
+++ programmers/Lv.3/숫자-타자-대회.py
@@ -0,0 +1,15 @@
+"""
+
+```yaml
+problem: "숫자 타자 대회"
+tags: 
+difficulty: LV3
+source: programmers
+link: https://school.programmers.co.kr/learn/courses/30/lessons/136797
+```
+
+"""
+
+def solution(numbers):
+    answer = 0
+    return answer
