c7c0b35b6357eb5c3de9d89db0f2680997ce6b8a
jadewisemann <jadewisemann@gmail.com>
2024-04-20 01:46:19 +0900

    question: add
    ```yaml
    problem: "Stop gninnipS My sdroW!"
    tags: STRINGS, ALGORITHMS
    difficulty: 6-kyu
    source: codewars
    link: https://www.codewars.com/kata/5264d2b162488dc400000001/
    ```

---
+++ codewars/6-kyu/Stop-gninnipS-My-sdroW.js
@@ -0,0 +1,13 @@
+/*
+```yaml
+problem: "Stop gninnipS My sdroW!"
+tags: STRINGS, ALGORITHMS
+difficulty: 6-kyu
+source: codewars
+link: https://www.codewars.com/kata/5264d2b162488dc400000001/
+```
+*/
+
+function spinWords(string){
+  //TODO Have fun :)
+}
