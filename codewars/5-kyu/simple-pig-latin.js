412b9a194681f9ccc1c12b0598aeb0cd4b52e9b0
jadewisemann <jadewisemann@gmail.com>
2024-04-20 23:21:05 +0900

    question: add new
    ```yaml
    problem: "Simple Pig Latin"
    tags: REGULAR EXPRESSIONS, ALGORITHMS
    difficulty: 5-kyu
    source: codewars
    link: https://www.codewars.com/kata/520b9d2ad5c005041100000f/
    ```

---
+++ codewars/5-kyu/simple-pig-latin.js
@@ -0,0 +1,11 @@
+```yaml
+problem: "Simple Pig Latin"
+tags: REGULAR EXPRESSIONS, ALGORITHMS
+difficulty: 5-kyu
+source: codewars
+link: https://www.codewars.com/kata/520b9d2ad5c005041100000f/
+```
+
+function pigIt(str){
+  //Code here
+}
