8692537e7f5115518617ee4d7bfd66577ad59a49
jadewisemann <jadewisemann@gmail.com>
2024-04-20 12:52:51 +0900

    question: add new
        ```yaml
        problem: "공 이동 시뮬레이션"
        tags:
        difficulty: LV3
        source: programmers
        link: https://school.programmers.co.kr/learn/courses/30/lessons/87391
        ```

---
+++ programmers/Lv.3/공-이동-시뮬레이션.py
@@ -0,0 +1,13 @@
+"""
+    ```yaml
+    problem: "공 이동 시뮬레이션"
+    tags: 
+    difficulty: LV3
+    source: programmers
+    link: https://school.programmers.co.kr/learn/courses/30/lessons/87391 
+    ```
+"""
+
+def solution(n, m, x, y, queries):
+    answer = -1
+    return answer
