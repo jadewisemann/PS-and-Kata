98ca9c5e08f6d5fce2cb51325f78655541213140
jadewisemann <jadewisemann@gmail.com>
2024-04-23 01:45:50 +0900

    solution: add #1.  use mathatical definition

--- programmers/Lv.2/점-찍기.py
+++ programmers/Lv.2/점-찍기.py
@@ -7,3 +7,9 @@ source: programmers
 link: https://school.programmers.co.kr/learn/courses/30/lessons/140107
 ```
 """
+
+def solution(k, d):
+    count = 0
+    for a in range(0, d//k + 1):
+        count += 1 + int(((d ** 2) - (a * k) ** 2) ** 0.5) // k
+    return count
