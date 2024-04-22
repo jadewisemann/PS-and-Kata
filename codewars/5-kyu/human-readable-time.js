97138a7366ac5287868d3977de6bcdf93064a89e
jadewisemann <jadewisemann@gmail.com>
2024-04-23 01:52:16 +0900

    question: add new
    ```
    problem: 'Human Readable Time'
    tags: DATE TIME, MATHEMATICS, ALGORITHMS
    difficulty: 5-kyu
    source: codewars
    link: https://www.codewars.com/kata/52685f7382004e774f0001f7/
    ```

---
+++ codewars/5-kyu/human-readable-time.js
@@ -0,0 +1,8 @@
+```
+problem: 'Human Readable Time'
+tags: DATE TIME, MATHEMATICS, ALGORITHMS
+difficulty: 5-kyu
+source: codewars
+link: https://www.codewars.com/kata/52685f7382004e774f0001f7/
+```
+
