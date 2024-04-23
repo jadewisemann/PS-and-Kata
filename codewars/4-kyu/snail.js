832289906e633b126f8cc33e361e96afc7fa68e0
jadewisemann <jadewisemann@gmail.com>
2024-04-24 03:23:12 +0900

    soltuon: add #2

--- codewars/4-kyu/snail.js
+++ codewars/4-kyu/snail.js
@@ -10,20 +10,14 @@ link: https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1
 
 
 
+
+
 const snail = array => {
   var result = [];
-  while (array.length > 0) {
-    result = [...result, ...array.shift()];
-    array.forEach(row => {
-      result.push(row.pop());
-    });
-    if (array.length > 0) {
-      result = [...result, ...array.pop().reverse()];
-    }
-    array.slice().reverse().forEach(row => {
-      result.push(row.shift());
-    });
+  while (array.length) {
+    result = [...result, ...array.shift()]
+    array.forEach(row => result.push(row.pop()))
+    array.reverse().forEach(row => row.reverse());
   }
   return result;
 }
-
