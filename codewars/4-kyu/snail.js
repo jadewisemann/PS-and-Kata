244da7e84b0b4725a94043ba6092c955763c00f1
jadewisemann <jadewisemann@gmail.com>
2024-04-24 03:06:23 +0900

    solution: add new

--- codewars/4-kyu/snail.js
+++ codewars/4-kyu/snail.js
@@ -7,3 +7,23 @@ source: codewars
 link: https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1
 ```
 */
+
+
+
+const snail = array => {
+  var result = [];
+  while (array.length > 0) {
+    result = [...result, ...array.shift()];
+    array.forEach(row => {
+      result.push(row.pop());
+    });
+    if (array.length > 0) {
+      result = [...result, ...array.pop().reverse()];
+    }
+    array.slice().reverse().forEach(row => {
+      result.push(row.shift());
+    });
+  }
+  return result;
+}
+
