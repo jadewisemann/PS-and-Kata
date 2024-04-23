27228c8ee745dc67c15906369fcc075ee9ee55c8
jadewisemann <jadewisemann@gmail.com>
2024-04-23 23:31:29 +0900

    question: add new
    """
    problem: '혼자서 하느 틱택토'
    tags:
    difficulty: LV2
    source: programmers
    link: https://school.programmers.co.kr/learn/courses/30/lessons/160585
    """

---
+++ programmers/Lv.2/혼자서-하는-틱택토.py
@@ -0,0 +1,11 @@
+"""
+problem: '혼자서 하느 틱택토'
+tags: 
+difficulty: LV2
+source: programmers
+link: https://school.programmers.co.kr/learn/courses/30/lessons/160585
+"""
+
+def solution(board):
+    answer = -1
+    return answer
