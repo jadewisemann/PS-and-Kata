9df42daeb7b6e9a68f4f0c37d4d68ad8501f3c63
jadewisemann <jadewisemann@gmail.com>
2024-04-25 22:46:23 +0900

    solution: add new

--- codewars/5-kyu/rgb-to-hex-conversion.js
+++ codewars/5-kyu/rgb-to-hex-conversion.js
@@ -7,3 +7,27 @@ source: codewars
 link: https://www.codewars.com/kata/513e08acc600c94f01000001
 ```
 */
+// const rgb = (r, g, b) => [r, g, b]
+//  .map((color) => (
+//     color < 0 ? 0 :
+//     color > 255 ? 255
+//     : color)
+//     .toString(16)
+//     .padStart(2, "0")
+//     .toUpperCase()
+//   ).join("")
+
+
+// const rgb = (r, g, b) => [r, g, b]
+//    .map((color) => Math.max(Math.min(255, color), 0)
+//     .toString(16)
+//     .padStart(2, "0")
+//     .toUpperCase()
+//   ).join("")
+
+const rgb = (...args) => args
+  .map((color) => Math.max(Math.min(255, color), 0)
+    .toString(16)
+    .padStart(2, "0")
+    .toUpperCase()
+  ).join("")
