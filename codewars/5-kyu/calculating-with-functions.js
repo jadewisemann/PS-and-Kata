65cd6441b763fb3c41689e0cb1cbecda7fb2eb3d
jadewisemann <jadewisemann@gmail.com>
2024-04-27 00:53:08 +0900

    solution: add new

--- codewars/5-kyu/calculating-with-functions.js
+++ codewars/5-kyu/calculating-with-functions.js
@@ -8,18 +8,19 @@ link: https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39
 ```
 */
 
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+const zero  = fn => fn ? fn(0) : 0;
+const one   = fn => fn ? fn(1) : 1;
+const two   = fn => fn ? fn(2) : 2;
+const three = fn => fn ? fn(3) : 3;
+const four  = fn => fn ? fn(4) : 4;
+const five  = fn => fn ? fn(5) : 5;
+const six   = fn => fn ? fn(6) : 6;
+const seven = fn => fn ? fn(7) : 7;
+const eight = fn => fn ? fn(8) : 8;
+const nine  = fn => fn ? fn(9) : 9;
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+const plus      = right => left => left + right 
+const minus     = right => left => left - right
+const times     = right => left => left * right
+const dividedBy = right => left => Math.floor(left / right)
+
