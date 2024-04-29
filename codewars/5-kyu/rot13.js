b86d693ef09666ea0f161d10a5c4ef49164c43f4
jadewisemann <jadewisemann@gmail.com>
2024-04-30 01:22:27 +0900

    solution: add #1 use if else

--- codewars/5-kyu/rot13.js
+++ codewars/5-kyu/rot13.js
@@ -7,3 +7,11 @@ source: codewars
 link: https://www.codewars.com/kata/530e15517bc88ac656000716
 ```
 */
+
+const rot13 = message => message.replace(
+  /[a-z]/gi, char => String.fromCharCode(
+    (char <= 'Z' ? 90 : 122) >= (char = char.charCodeAt(0) + 13)
+      ? char
+      : char - 26)
+  );
+
