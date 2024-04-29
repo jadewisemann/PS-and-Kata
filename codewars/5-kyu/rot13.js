d6cb0495c08a3939ebd22c6b443c757687b6c68c
jadewisemann <jadewisemann@gmail.com>
2024-04-30 01:32:05 +0900

    soltuion: add #2

--- codewars/5-kyu/rot13.js
+++ codewars/5-kyu/rot13.js
@@ -8,10 +8,18 @@ link: https://www.codewars.com/kata/530e15517bc88ac656000716
 ```
 */
 
-const rot13 = message => message.replace(
-  /[a-z]/gi, char => String.fromCharCode(
-    (char <= 'Z' ? 90 : 122) >= (char = char.charCodeAt(0) + 13)
-      ? char
-      : char - 26)
-  );
 
+// const rot13 = message => message.replace(
+//   /[a-z]/gi, char => String.fromCharCode(
+//     (char <= 'Z' ? 90 : 122) >= (char = char.charCodeAt(0) + 13)
+//       ? char
+//       : char - 26)
+//   );
+
+
+
+const rot13 = message => message.replace(/[a-z]/gi,
+  char => String.fromCharCode(
+    char.charCodeAt(0) + (char.toLowerCase() <= 'm'? +1 : -1) * 13
+  )
+)
