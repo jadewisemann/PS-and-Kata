da3b38ff87e6fd5a0da111e36e2badd0b38e2e29
jadewisemann <jadewisemann@gmail.com>
2024-05-01 01:58:35 +0900

    solution: add #1

--- codewars/5-kyu/pete-the-baker.js
+++ codewars/5-kyu/pete-the-baker.js
@@ -8,5 +8,11 @@ link: https://www.codewars.com/kata/525c65e51bf619685c000059
 ```
 */
 
-
+const  cakes = (recipe, available) => {
+  const minArr = []
+  Object.entries(recipe).forEach( ([key, value]) => {
+    minArr.push(Math.floor((available[key] ? available[key] : 0) / value))
+  })
+  return Math.min(...minArr)
+}
 
