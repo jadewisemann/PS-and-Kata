3b3ecee3aaa0cc1bd0a087608a5a4eed42e73473
jadewisemann <jadewisemann@gmail.com>
2024-05-01 00:02:27 +0900

    kata: add new
    ```yaml
    problem: 'The Hashtag Generator'
    tags: STRINGS, ALGORITHMS
    difficulty: 5-kyu
    source: codewars
    link: https://www.codewars.com/kata/52449b062fb80683ec000024
    ```

---
+++ codewars/5-kyu/the-hashtag-generator.js
@@ -0,0 +1,12 @@
+
+
+/*
+```yaml
+problem: 'The Hashtag Generator'
+tags: STRINGS, ALGORITHMS
+difficulty: 5-kyu
+source: codewars
+link: https://www.codewars.com/kata/52449b062fb80683ec000024
+```
+*/
+
