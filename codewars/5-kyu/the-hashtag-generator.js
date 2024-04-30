8aa08c855fac797b4d4a666f7f55bd6906287968
jadewisemann <jadewisemann@gmail.com>
2024-05-01 00:25:09 +0900

    solution: add #1.

--- codewars/5-kyu/the-hashtag-generator.js
+++ codewars/5-kyu/the-hashtag-generator.js
@@ -1,5 +1,3 @@
-
-
 /*
 ```yaml
 problem: 'The Hashtag Generator'
@@ -9,4 +7,14 @@ source: codewars
 link: https://www.codewars.com/kata/52449b062fb80683ec000024
 ```
 */
+const generateHashtag = (str) => {
+  const trimmedStr = str.trim();
+  if (!trimmedStr) return false;
 
+  const hashtag = `#${trimmedStr
+    .split(/\s+/)
+    .map(word => word.replace(/^\S/, c => c.toUpperCase()))
+    .join('')}`;
+  
+  return hashtag.length > 140 ? false : hashtag;
+};
