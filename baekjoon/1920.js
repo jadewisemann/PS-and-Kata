c70c3b03f605b26ab1ea40551677e06696886fab
jadewisemann <jadewisemann@gmail.com>
2024-11-25 14:48:35 +0900

    solution: add 1920 #2

--- baekjoon/1920.js
+++ baekjoon/1920.js
@@ -5,24 +5,10 @@ const filePath =
 
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-const n = Number(input[0]);
-
 const [A, B] = [input[1], input[3]].map(str => str.split(' ').map(Number));
 A.sort((a, b) => a - b)
 
-const binarySearch = (arr, target, start = 0, end = n - 1) => {
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    
-    if (arr[mid] === target) return 1;
-
-    target > arr[mid]
-      ? start = mid + 1
-      : end = mid - 1;
-  }
-  return 0;
-};
-
-console.log(B.map(b => binarySearch(A, b)).join('\n'));
-
+const aArr = new Set(A);
+const answer = B.map( b => aArr.has(b) ? 1 : 0);
+console.log(answer.join('\n'))
 
